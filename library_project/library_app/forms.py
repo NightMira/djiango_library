19a9aceb9fc76e262106eedee1e3308862e40beb
MirGame <mirgame@icloud.com>
2026-04-24 01:06:13 +0300

    v2

--- library_project/library_app/forms.py
+++ library_project/library_app/forms.py
@@ -1,8 +1,65 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 
 class RegisterForm(UserCreationForm):
+    """
+    Форма регистрации пользователя:
+    - username (login)
+    - email
+    - full_name
+    - phone
+    - password1 / password2
+    """
+
+    email = forms.EmailField(required=True)
+    full_name = forms.CharField(required=True)
+    phone = forms.CharField(required=False)
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'full_name', 'phone', 'password1', 'password2')
+        fields = (
+            'username',
+            'email',
+            'full_name',
+            'phone',
+            'password1',
+            'password2'
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Bootstrap оформление всех полей
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'form-control',
+                'placeholder': field.label
+            })
+
+    def clean_email(self):
+        """
+        Проверка уникальности email
+        """
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Пользователь с таким email уже существует")
+
+        return email
+
+    def save(self, commit=True):
+        """
+        Сохранение пользователя
+        """
+        user = super().save(commit=False)
+
+        user.email = self.cleaned_data.get('email')
+        user.full_name = self.cleaned_data.get('full_name')
+        user.phone = self.cleaned_data.get('phone') or ""
+
+        if commit:
+            user.save()
+
+        return user
--- library_project/library_app/urls.py
+++ library_project/library_app/urls.py
@@ -3,12 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+
     path("book/<str:isbn>/", views.book_detail, name="book_detail"),
 
-    path("login/", views.user_login, name="login"),
-    path("logout/", views.user_logout, name="logout"),
-
     path("profile/", views.profile, name="profile"),
+
     path("borrow/<str:isbn>/", views.borrow_book, name="borrow_book"),
 
     # библиотекарь
@@ -16,6 +15,5 @@ urlpatterns = [
     path("librarian/issue/", views.issue_book, name="issue_book"),
     path("librarian/return/<int:loan_id>/", views.return_book, name="return_book"),
 
-    path('register/', views.register_view, name='register'),
-    path('profile/', views.profile, name='profile'),
+    path("register/", views.register_view, name="register"),
 ]
